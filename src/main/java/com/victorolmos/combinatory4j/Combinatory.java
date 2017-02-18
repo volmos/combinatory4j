7d8d1f857919285e10196e0cc87ee7d061ece459
volmos <victor.olmos.aleman@gmail.com>
2017-02-18 16:02:56 +0100

    Static methods Combinatory class jdoc

--- src/main/java/com/victorolmos/combinatory4j/Combinatory.java
+++ src/main/java/com/victorolmos/combinatory4j/Combinatory.java
@@ -13,8 +13,20 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Class with static methods for calculate different combinatory groups.
+ * This methods retunrs the results without use callback.
+ * Prevent to use this methods when the number of results groups has very
+ * large to reduce memory usage.
+ */
 public class Combinatory {
 
+    /**
+     * Generates permutations of a set of elements
+     *
+     * @param elements set containig elements to calculate permutations
+     * @return the collection of permutations
+     */
     public static <T> Collection<List<T>> generatePermutations(final Set<T> elements) {
         final ArrayList<List<T>> permutationsResults = new ArrayList<>();
         final PermutationsGenerator<T> permutationsGenerator = new PermutationsGenerator<>();
@@ -28,6 +40,13 @@ public class Combinatory {
         return permutationsResults;
     }
 
+    /**
+     * Generates variations of a set of elements
+     *
+     * @param elements set containig elements to calculate variations
+     * @param groupSize size of result variations
+     * @return the collection of variations
+     */
     public static <T> Collection<List<T>> generateVariations(final Set<T> elements, final int groupSize) {
         final ArrayList<List<T>> variationsResults = new ArrayList<>();
         final VariationsGenerator<T> variationsGenerator = new VariationsGenerator<>();
@@ -42,6 +61,13 @@ public class Combinatory {
         return variationsResults;
     }
 
+    /**
+     * Generates variations with repetitions of a set of elements
+     *
+     * @param elements set containig elements to calculate variations
+     * @param groupSize size of result variations
+     * @return the collection of variations with repetitions
+     */
     public static <T> Collection<List<T>> generateVariationsWithRepetitions(final Set<T> elements, final int groupSize) {
         final ArrayList<List<T>> variationsResults = new ArrayList<>();
         final VariationsWithRepetitionsGenerator<T> variations = new VariationsWithRepetitionsGenerator<>();
@@ -56,6 +82,13 @@ public class Combinatory {
         return variationsResults;
     }
 
+    /**
+     * Generates variations of a collection of elements. The collection may have repeated elements
+     *
+     * @param elements collection containing elements to calculate variations
+     * @param groupSize size of result variations
+     * @return the collection of variations
+     */
     public static <T> Collection<List<T>> generateVariationsWithRepeatElements(final Collection<T> elements, final int groupSize) {
         final ArrayList<List<T>> variationsResults = new ArrayList<>();
         final RepeatElementsVariationsGenerator<T> variationsGenerator = new RepeatElementsVariationsGenerator<>();
@@ -70,6 +103,13 @@ public class Combinatory {
         return variationsResults;
     }
 
+    /**
+     * Generates combinations of a set of elements
+     *
+     * @param elements set containig elements to calculate combinations
+     * @param groupSize size of result combinations
+     * @return the collection of combinations
+     */
     public static <T> Collection<Collection<T>> generateCombinations(final Set<T> elements, final int groupSize) {
         final ArrayList<Collection<T>> combinationsResults = new ArrayList<>();
         final CombinationsGenerator<T> combinationsGenerator = new CombinationsGenerator<>();
@@ -84,6 +124,13 @@ public class Combinatory {
         return combinationsResults;
     }
 
+    /**
+     * Generates combinations with repetitions of a set of elements
+     *
+     * @param elements set containig elements to calculate combinations
+     * @param groupSize size of result combinations
+     * @return the collection of combinations with repetitions
+     */
     public static <T> Collection<Collection<T>> generateCombinationsWithRepetitions(final Set<T> elements, final int groupSize) {
         final ArrayList<Collection<T>> combinationsResults = new ArrayList<>();
         final CombinationsWithRepetitionsGenerator<T> combinationsWithRepetitionsGenerator = new CombinationsWithRepetitionsGenerator<>();
@@ -98,6 +145,13 @@ public class Combinatory {
         return combinationsResults;
     }
 
+    /**
+     * Generates combinations of a collection of elements. The collection may have repeated elements
+     *
+     * @param elements collection containing elements to calculate combinations
+     * @param groupSize size of result combinations
+     * @return the collection of combinations
+     */
     public static <T> Collection<Collection<T>> generateCombinationsWithRepeatElements(final Collection<T> elements, final int groupSize) {
         final ArrayList<Collection<T>> combinationsResults = new ArrayList<>();
         final RepeatElementsCombinationsGenerator<T> combinationsGenerator = new RepeatElementsCombinationsGenerator<>();
